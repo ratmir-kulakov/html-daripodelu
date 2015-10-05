32f02bb1abff5624aaf5c49213eb021a64a853b2
Ratmir Kulakov <ratmir.kulakov@gmail.com>
2015-10-05 22:49:06 +0300

    Added fields for upload files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    if($(".news-list-box").length){
+    if( $(".news-list-box").length ){
         $(".news-list-box").bxSlider({
             mode: 'vertical',
             infiniteLoop: false,
@@ -9,7 +9,7 @@ $(function(){
         });
     }
 
-    if($("select.choice")) {
+    if( $("select.choice") ) {
         $("select.choice").select2({
             //allowClear: true,
             width: "resolve",
@@ -17,4 +17,23 @@ $(function(){
         });
     }
 
+    if( $(".input-file-box").length ) {
+        $(".input-file-box .file-field").click(function(){
+            var inputTxt = $(this),
+                file = inputTxt.parent().find("input[type='file']");
+
+            file.click();
+            file.change(function(){
+                inputTxt.val(file.val());
+            });
+        });
+
+        $(".input-file-box .delete-item").click(function(){
+            $(this).parents(".input-file-item").eq(0).remove();
+        });
+
+        $(".input-file-box .attach-item").click(function(){
+            $(this).parents(".input-file-box").eq(0).append("");
+        });
+    }
 });
