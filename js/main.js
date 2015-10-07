5f46d17446a5166568de9decf942797d51d88115
Ratmir Kulakov <ratmir.kulakov@gmail.com>
2015-10-07 09:40:21 +0300

    Added js code for cart.html page

--- js/main.js
+++ js/main.js
@@ -19,21 +19,53 @@ $(function(){
 
     if( $(".input-file-box").length ) {
         $(".input-file-box .file-field").click(function(){
-            var inputTxt = $(this),
-                file = inputTxt.parent().find("input[type='file']");
-
-            file.click();
-            file.change(function(){
-                inputTxt.val(file.val());
-            });
+            fileDialogOpen(this);
         });
 
         $(".input-file-box .delete-item").click(function(){
-            $(this).parents(".input-file-item").eq(0).remove();
+            removeFileInput(this);
         });
 
         $(".input-file-box .attach-item").click(function(){
-            $(this).parents(".input-file-box").eq(0).append("");
+            addFileInput(this);
         });
     }
 });
+
+function removeFileInput(target) {
+    $(target).parents(".input-file-item").eq(0).remove();
+}
+
+function addFileInput(target){
+    var spanAttachItem = $("<span />"),
+        spanDeleteItem = $("<span />"),
+        inputTxt = $("<input>");
+
+    spanDeleteItem.addClass("img-btn delete-item").attr("title", "Удалить").click(function(){ removeFileInput(this); });
+    $(target).parents(".input-file-item").eq(0).append(spanDeleteItem);
+
+    spanAttachItem.addClass("img-btn attach-item").attr("title", "Прикрепить еще");
+    spanAttachItem.click(function(){ addFileInput(this) });
+
+    inputTxt.addClass("file-field").attr("type", "text").click(function(){ fileDialogOpen(this); });
+
+    $(target).parents(".input-file-box").eq(0).append(
+        $("<div />").addClass("input-file-item").append(
+            inputTxt,
+            spanAttachItem,
+            $("<input>").addClass("file-field").attr({"type": "file", "name": "attached_file[]"})
+        )
+    );
+
+    $(target).remove();
+}
+
+function fileDialogOpen(target){
+    var inputTxt = $(target),
+        file = inputTxt.parent().find("input[type='file']");
+
+    file.click();
+    file.change(function(){
+        inputTxt.val(file.val());
+    });
+}
